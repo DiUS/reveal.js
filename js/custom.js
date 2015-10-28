b45b4aac6e24e9e445e7733b00607506e6fbbdc0
Voon Siong Wong <vwong@dius.com.au>
2015-10-28 12:16:44 +1100

    Use markdown examples

--- js/custom.js
+++ js/custom.js
@@ -15,6 +15,8 @@ Reveal.initialize({
 
   dependencies: [
     { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+    { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
     { src: 'plugin/zoom-js/zoom.js', async: true },
     { src: 'plugin/notes/notes.js', async: true }
   ]
