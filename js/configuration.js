4632b467dfa7b21a9313b267b01ef92170be495d
Voon Siong Wong <vwong@dius.com.au>
2015-10-28 14:40:48 +1100

    Use CSS classes to change theme

---
+++ js/configuration.js
@@ -0,0 +1,23 @@
+Reveal.initialize({
+
+  width: 960,
+  height: 700,
+  margin: 0.1,
+  minScale: 0.2,
+  maxScale: 1.5,
+
+  controls: false,
+  progress: true,
+  history: true,
+  center: false,
+  transition: 'slide', // none/fade/slide/convex/concave/zoom
+  transitionSpeed: 'fast',
+
+  dependencies: [
+    { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+    { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    { src: 'plugin/zoom-js/zoom.js', async: true },
+    { src: 'plugin/notes/notes.js', async: true }
+  ]
+});
